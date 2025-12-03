9abbd56c3e70df6a504cc682e593cb12dab54116
VeronicaScol <veronica.scolari@icloud.com>
2025-12-03 14:35:17 +0000

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
+
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Project Framingham Heart Disease - Group 7")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "The Framingham Heart Study is a long-term study of cardiovascular disease that identified risks factors and tehir join effects." \
+    " There are 4434 patients, each of them with 3 examination periods every 6 years. The outcome is: angina pectoris, myocardial infraction, Atherothrombotic Infarction or Cerebral Hemorrhage (Stroke) or death."
 )
+
